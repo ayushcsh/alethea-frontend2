8f818daf41515da18c4d16d74d3b302fc53892ff
Ayush Kumar <ayushkumar.nov.2005@gmail.com>
2025-09-08 00:19:00 +0530

    Fix Vercel deployment issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@tailwindcss/typography']
+  },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { canvas: 'canvas' }]; // Add this line
+    return config;
+  },
+};
 
 export default nextConfig;
